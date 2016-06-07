de3cf85f8a928afbf673457c559134ecc2550a4b
srp2930 <s.rimingtonpounder@gmail.com>
2016-06-07 14:45:40 +0100

    added shotgun and new routes

--- app.rb
+++ app.rb
@@ -1,5 +1,24 @@
 require 'sinatra'
+require 'shotgun'
+
+set :session_secret, 'super secret'
 
 get '/' do
   "hello!"
+end
+
+get '/secret' do
+  "ALEX IS COOL!"
+end
+
+get '/rumpel' do
+  "Spinning gold!"
+end
+
+get '/cinderella' do
+  "Lost a shoe"
+end
+
+get '/cinderella/princec' do
+  "found some glass bro"
 end
